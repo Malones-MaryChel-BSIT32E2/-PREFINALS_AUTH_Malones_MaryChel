e4d80d374cb51d1267cf394793a60228a698a71f
Domingo-Jhana Christina L.-BSIT 32E2 <148880134+Domingo-JhanaChristina-BSIT32E2@users.noreply.github.com>
2024-06-30 22:30:48 +0800

    Add project files.

---
+++ MALONES_PRE-FINALS_ASSIGNMENT-1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MALONES_PRE_FINALS_ASSIGNMENT_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
